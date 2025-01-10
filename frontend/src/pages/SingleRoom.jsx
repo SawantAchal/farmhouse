f1137e473fbd54b8f171b35dbeff81b285bacf6e
SawantAchal <achals1718@gmail.com>
2025-01-10 19:13:06 +0530

    update details

--- frontend/src/pages/SingleRoom.jsx
+++ frontend/src/pages/SingleRoom.jsx
@@ -45,8 +45,8 @@ const SingleRoom = () => {
           </article>
           <article className="info">
             <h3>info</h3>
-            <h6>price: ${price}</h6>
-            <h6>size: ${size} SQFT</h6>
+            <h6>price: Rs.{price}</h6>
+            <h6>size: {size} SQFT</h6>
             <h6>max capacity: {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
             <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
             <h6>{breakfast && 'free breakfast included'}</h6>
