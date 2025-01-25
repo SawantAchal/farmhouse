55cd0540c608ceaf15a4275cabcad8ba5d035363
SawantAchal <achals1718@gmail.com>
2025-01-25 22:29:56 +0530

    update logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { FaAlignRight } from 'react-icons/fa';
 import {Link} from 'react-router-dom'
-import logo from '../assets/logo.svg';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
